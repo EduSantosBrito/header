65b3897ba77385e2be9820e7dbefd6a2ecb97b59
EduSantosBrito <edu.santos.brito@gmail.com>
2021-06-17 19:48:40 -0300

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import PropTypes from "prop-types";
+import routes from "@brito/routes";
+import { useEffect } from "react";
 
 const App = ({ name }) => {
-  return <div>Test {name}</div>;
+  useEffect(() => {
+    console.info(routes);
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {routes.map((route) => (
+          <>
+            <a href={route.url}>
+              <li>{route.label}</li>
+            </a>
+            {route.subroutes?.length && (
+              <ul>
+                {route.subroutes?.map((subroute) => (
+                  <a href={`${route.url}${subroute.url}`}>
+                    <li>{subroute.label}</li>
+                  </a>
+                ))}
+              </ul>
+            )}
+          </>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 App.propTypes = {
